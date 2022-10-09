8fe0a7e17ff7bdccb010eb7dd619f85ddaf0cce8
khalid.hussain <khalid.hussain@foodsby.com>
2022-10-09 00:12:55 -0500

    "#2: Adds ability to check whether a given product is in stock, by its sku code"

--- api-inventory-service/src/main/kotlin/com/shopping/inventory/controller/InventoryController.kt
+++ api-inventory-service/src/main/kotlin/com/shopping/inventory/controller/InventoryController.kt
@@ -1,9 +1,20 @@
 package com.shopping.inventory.controller
 
+import com.shopping.inventory.service.InventoryService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/inventory")
-class InventoryController {
+class InventoryController(private val inventoryService: InventoryService) {
+
+    @GetMapping("{skuCode}")
+    @ResponseStatus(HttpStatus.OK)
+    fun isInStock(@PathVariable("skuCode") skuCode: String): Boolean {
+        return inventoryService.isInStock(skuCode)
+    }
 }
