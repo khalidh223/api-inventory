6f31af783949a5467885161d3695918f131f728b
Khalid Hussain <khalid.hussain@foodsby.com>
2022-10-17 22:54:10 -0500

    Adds support for service discovery via Eureka

--- api-inventory-service/src/main/kotlin/com/shopping/inventory/ApiInventoryApplication.kt
+++ api-inventory-service/src/main/kotlin/com/shopping/inventory/ApiInventoryApplication.kt
@@ -2,8 +2,10 @@ package com.shopping.inventory
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class ApiInventoryApplication
 
 fun main(args: Array<String>) {
