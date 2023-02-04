c189aa0a059ad4ee6ce92726690d3c54c50f0d17
Khalid Hussain <khalid.hussain@foodsby.com>
2023-02-04 12:37:00 -0600

    Adds retrofit config & publish task, also configures spring to update schema of DB when there are changes

--- api-inventory-service/src/main/kotlin/com/shopping/inventory/controller/InventoryController.kt
+++ api-inventory-service/src/main/kotlin/com/shopping/inventory/controller/InventoryController.kt
@@ -1,11 +1,13 @@
 package com.shopping.inventory.controller
 
+import com.shopping.inventory.model.InventoryRequest
 import com.shopping.inventory.service.InventoryService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
@@ -19,9 +21,9 @@ class InventoryController(private val inventoryService: InventoryService) {
         return inventoryService.productIsInStock(skuCode)
     }
 
-    @GetMapping()
+    @PostMapping()
     @ResponseStatus(HttpStatus.OK)
-    fun productsAreInStock(@RequestParam skuCode: List<String>): Boolean {
-        return inventoryService.productsAreInStock(skuCode)
+    fun productsAreInStock(@RequestBody request: InventoryRequest): Boolean {
+        return inventoryService.productsAreInStock(request.skuCodeList)
     }
 }
